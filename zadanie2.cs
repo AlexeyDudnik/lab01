9e6d1b1977277bea1985809de2f1f0afc0d65e38
AlexDudnik <145279680+AlexeyDudnik@users.noreply.github.com>
2024-09-12 15:33:58 +0300

    Update zadanie2.cs

--- zadanie2.cs
+++ zadanie2.cs
@@ -1,1 +1,38 @@
-
+using System;
+class Rectangle
+{
+    private double SideA;
+    private double SideB;
+    public Rectangle(double sideA, double sideB)
+    {
+        this.SideA = sideA;
+        this.SideB = sideB;
+    }
+    private double CalcArea()
+    {
+        return SideA * SideB;
+    }
+    private double CalcPerimetr()
+    {
+        return 2*(SideB + SideA);
+    }
+    public double Area
+    {
+        get { return CalcArea(); }
+    }
+    public double Perimetr
+    {
+        get { return CalcPerimetr(); }
+    }
+}
+class Progr
+{
+    static void Main()
+    {
+        double SideA = double.Parse(Console.ReadLine());
+        double SideB = double.Parse(Console.ReadLine());
+        Rectangle rectangle = new Rectangle(SideA, SideB);
+        Console.WriteLine(rectangle.Area);
+        Console.WriteLine(rectangle.Perimetr);
+    }
+}
