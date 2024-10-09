8df4d7bb9e0d53e78adcbf82dd4d10e848421557
AlexDudnik <145279680+AlexeyDudnik@users.noreply.github.com>
2024-10-09 16:26:24 +0300

    Update zadanie3.cs

--- zadanie3.cs
+++ zadanie3.cs
@@ -1,82 +1,61 @@
 using System;
-using System.Reflection.Metadata;
-
-public class Point
+class Point
 {
-    private double X;
-    private double Y;
-    public Point(double X, double Y)
+    private int x;
+    private int y;
+    public int X { get { return x; } }
+    public int Y { get { return y; } }
+    public Point(int x, int y)
     {
-        this.X = X;
-        this.Y = Y;
-    }
-    public double PointX
-    {
-        get { return X; }
-    }
-    public double PointY
-    {
-        get { return Y; }
+        this.x = x;
+        this.y = y;
     }
 }
-public class Figure
+class Figure
 {
     private Point[] points;
-    public string Name {get;set;}
+    public string Name { get; set; }
+    public Figure(Point p1, Point p2, Point p3) : this(p1, p2, p3, null, null) { }
+    public Figure(Point p1, Point p2, Point p3, Point p4) : this(p1, p2, p3, p4, null) { }
     public Figure(Point p1, Point p2, Point p3, Point p4, Point p5)
     {
         points = new Point[] { p1, p2, p3, p4, p5 };
-        if (p5 == null)
-        {
-            Name = "треугольник";
-        }
-        else if (p4 == null)
-        {
-            Name = "четырехугольник";
-        }
-        else
-        {
-            Name = "пятиугольник";
-        }
-    }
-    public Figure(Point p1, Point p2, Point p3) : this(p1, p2, p3, null, null)
-    {
-        Name = "треугольник";
-    }
-    public Figure(Point p1, Point p2, Point p3,Point p4) : this(p1, p2, p3, p4, null)
-    {
-        Name = "четырехугольник";
+        Name = "Многоугольник";
     }
     public double LengthSide(Point A, Point B)
     {
-        return Math.Sqrt(Math.Pow(B.PointX - A.PointX, 2) + Math.Pow(B.PointY - A.PointY, 2));
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
     }
     public double PerimeterCalculator()
     {
         double perimeter = 0;
-        for (int i = 0; i < points.Length - 1; i++)
+        int validPointsCount = 0;
+        foreach (var point in points)
         {
-            if (points[i] != null && points[i + 1] != null)
+            if (point != null)
             {
-                perimeter += LengthSide(points[i], points[i + 1]);
+                validPointsCount++;
             }
         }
-        if (points[0] != null && points[points.Length - 1] != null)
+        for (int i = 0; i < validPointsCount - 1; i++)
         {
-            perimeter += LengthSide(points[points.Length - 1], points[0]);
+            perimeter += LengthSide(points[i], points[i + 1]);
         }
+        perimeter += LengthSide(points[validPointsCount - 1], points[0]);
+
         return perimeter;
     }
 }
-class Progr
+class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Point p1 = new Point(1, 1);
-        Point p2 = new Point(2, 2);
-        Point p3 = new Point(3, 3);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(4, 0);
+        Point p3 = new Point(4, 3);
         Figure triangle = new Figure(p1, p2, p3);
-        Console.WriteLine(triangle.Name);
-        Console.WriteLine(triangle.PerimeterCalculator());
+        triangle.Name = "Треугольник";
+        Console.WriteLine($"Название фигуры: {triangle.Name}");
+        Console.WriteLine($"Периметр фигуры: {triangle.PerimeterCalculator()}");
     }
 }
